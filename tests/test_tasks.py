0424d32530a23db63c920e7a7b7ce6b4c585dfc2
Brett Anda <brett@brettanda.ca>
2022-07-18 01:53:39 -0600

    fixed the tests for every hour of the day

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -9,11 +9,27 @@ from cogs.tasks import Task
 pytestmark = pytest.mark.asyncio
 
 
+def boundedIncrement(num: int, _max: int):
+    num = num + 1
+    if num > _max:
+        return 0, True
+    return num, False
+    # return ((num + 1) + (_max + 1)) % (_max + 1)
+
+
+def boundedDecrement(num: int, _min: int = 0, wrap_to: int = 10):
+    num = num - 1
+    if num < _min:
+        return wrap_to, True
+    return num, False
+    # return ((num - 1) - (_min - 1)) % (_min - 1)
+
+
 async def test_task_from_past():
     now = discord.utils.utcnow().replace(tzinfo=None)
-    hour = now.hour - 1 if now.hour - 1 >= 0 else 23
+    hour, wraped = boundedDecrement(now.hour, 0, 23)
     time = datetime.time(hour=hour)
-    date = datetime.datetime.combine(now.date() if hour != 23 else now.date() - datetime.timedelta(days=1), time)
+    date = datetime.datetime.combine(now.date() if not wraped else now.date() - datetime.timedelta(days=1), time)
     delta = datetime.timedelta(days=1)
     record = {
         "id": None,
@@ -36,10 +52,10 @@ async def test_task_from_past():
 
 async def test_task_from_past_plus_interval():
     now = discord.utils.utcnow().replace(tzinfo=None)
-    hour = now.hour - 1 if now.hour - 1 >= 0 else 23
+    hour, wraped = boundedDecrement(now.hour, 0, 23)
     time = datetime.time(hour=hour)
     delta = datetime.timedelta(days=1)
-    date = datetime.datetime.combine(now.date() if hour != 23 else now.date() - delta, time)
+    date = datetime.datetime.combine(now.date() if not wraped else now.date() - delta, time)
     record = {
         "id": None,
         "user_id": 215227961048170496,
@@ -61,10 +77,10 @@ async def test_task_from_past_plus_interval():
 
 async def test_task_hour_from_past():
     now = discord.utils.utcnow().replace(tzinfo=None)
-    hour = now.hour - 1 if now.hour - 1 >= 0 else 23
+    hour, wraped = boundedDecrement(now.hour, 0, 23)
     time = datetime.time(hour=hour)
     delta = datetime.timedelta(days=1)
-    date = datetime.datetime.combine(now.date() if hour != 23 else now.date() - delta, time)
+    date = datetime.datetime.combine(now.date() if not wraped else now.date() - delta, time)
     record = {
         "id": None,
         "user_id": 215227961048170496,
@@ -86,10 +102,10 @@ async def test_task_hour_from_past():
 
 async def test_task_hour_from_past_plus_interval():
     now = discord.utils.utcnow().replace(tzinfo=None)
-    hour = now.hour - 1 if now.hour - 1 >= 0 else 23
+    hour, wraped = boundedDecrement(now.hour, 0, 23)
     time = datetime.time(hour=hour)
     delta = datetime.timedelta(hours=1)
-    date = datetime.datetime.combine(now.date() if hour != 23 else now.date() - delta, time)
+    date = datetime.datetime.combine(now.date() if not wraped else now.date() - delta, time)
     record = {
         "id": None,
         "user_id": 215227961048170496,
@@ -111,10 +127,10 @@ async def test_task_hour_from_past_plus_interval():
 
 async def test_task_from_future():
     now = discord.utils.utcnow().replace(tzinfo=None)
-    hour = now.hour + 1 if now.hour + 1 < 24 else 0
+    hour, wraped = boundedIncrement(now.hour, 23)
     time = datetime.time(hour=hour)
     delta = datetime.timedelta(hours=1)
-    date = datetime.datetime.combine(now.date() if hour != 0 else now.date() + delta, time)
+    date = datetime.datetime.combine(now.date() if not wraped else now.date() + datetime.timedelta(days=1), time)
     record = {
         "id": None,
         "user_id": 215227961048170496,
